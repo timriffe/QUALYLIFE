735b679f82ee8af9a34d440f19e56466e97da0d0
timriffe <tim.riffe@gmail.com>
2022-03-14 13:27:02 +0100

    first commit

---
+++ data_prep.R
@@ -0,0 +1,418 @@
+source("functions.R")
+# Individual-level covariates for > 1million people
+Per <- read_dta("Data/MCVL2020PERSONA.dta") %>% 
+  dplyr::filter(sexe > 0) %>% 
+  mutate(IPF = as.integer(ident),
+         # get proper dates
+         dob = lubridate::dmy(paste("15",sprintf("%02d",mesnac),anynac, sep = ".")),
+         any_mort = ifelse(any_mort == 0, NA, any_mort),
+         dod = lubridate::dmy(paste("15",sprintf("%02d",mes_mort),any_mort, sep = ".")),
+         
+         # group education categories
+         estudis = as.integer(estudis),
+         edu = case_when(between(estudis,10,21) ~ "sin estudios",
+                         between(estudis,22,31) ~ "primarios",
+                         between(estudis,32, 42) ~ "secundarios",
+                         between(estudis,43,48) ~ "universitarios",
+                         TRUE ~ "desconocido")) %>% 
+  select(IPF, dob, sex = sexe, edu, dod, anynac)
+
+# employment trajectories to be merged with the above
+Cot <- read_dta("Data/COTITZACIONS2020.dta",
+                # We explicitly select particular columns
+                # There are more columns that could also be explored in the
+                # future
+                col_select = c(IPF,
+                               regimen, # we'll filter to regimen general
+                               grupo,
+                               contrato,
+                               parcial,
+                               alta, # date
+                               baja,  # date
+                               causa,
+                               CCC,
+                               CNAE_2009,
+                               CNAE93,
+                               trabajadores,
+                               TRL
+                               ),
+                n_max = 1e6) %>% 
+  # get date formats
+  mutate(alta = ymd(alta),
+         baja = ymd(baja),
+         baja = if_else(year(baja) > 2021, NA_Date_, baja),
+         duration = time_length(interval(alta,baja),unit="years"),
+         duration = ifelse(is.na(baja), 2021 - decimal_date(alta), duration)) %>% 
+  dplyr::filter(!is.na(alta)) 
+
+# Join and keep all rows where we have a matching person id (IPF)
+LC1 <- 
+  Per %>% 
+  inner_join(Cot, by = "IPF") %>% 
+  # filter to general regimen workers 
+  # (could this explain some gaps that are later padded?)
+  dplyr::filter(between(regimen, 111, 150)) %>% 
+  select(-regimen) %>% 
+  # group occupation categories
+  mutate(occ = case_when(
+    between(grupo, 1,3) ~ "skilled non manual",
+    between(grupo, 4, 7) ~ "unskilled non manual",
+    between(grupo, 8, 9) ~ "skilled manual",
+    between(grupo, 10, 11) ~ "unskilled manual",
+    grupo == 0 ~ "no consta",
+    grupo >= 12 ~ "16 years old" # also other ways to end 
+                                 # up here, merely indicative
+                                 # don't use as age filter!
+  )) %>% 
+  select(-grupo) %>% 
+  # Create main contract indicator: temp, perm, and unemp
+  mutate(temp = case_when(
+    contrato == 0 & between(TRL,700,799) ~ "unemp",
+    #contrato == 0 & (TRL < 700 | TRL >= 800) ~ "no consta",
+    between(contrato, 1, 3) ~ "perm", # permanent
+    contrato == 9 ~ "perm",
+    contrato == 20 ~ "perm",
+    contrato == 28 ~ "perm",
+    contrato == 40 ~ "perm",
+    contrato == 35 ~ "perm",
+    contrato == 42 ~ "perm",
+    between(contrato, 44, 49) ~ "perm",
+    between(contrato, 60, 61) ~ "perm",
+    contrato == 69 ~ "perm",
+    between(contrato, 70,71) ~ "perm",
+    contrato == 100 ~ "perm",
+    between(contrato, 109, 157) ~ "perm",
+    between(contrato, 189, 389) ~ "perm",
+    duration > 4 ~ "perm",
+    is.na(baja) ~ "no consta",
+    contrato == 0 & (TRL < 700 | TRL >= 800) ~ "no consta",
+    TRUE ~ "temp"))%>% 
+  select(-contrato) %>% 
+  
+  # turn work time per mil into a binary for full and part
+  # however, we may reconsider keeping the full info if needed
+  # to aggregate to marginality classes (short contracts / low %, etc)
+  mutate(part = case_when(
+    parcial == 0 ~ "full",
+    parcial >= 800 ~ "full",
+    TRUE ~ "part"
+  )) %>% 
+  select(-parcial) %>% 
+  # group sector categories. Knarly recoding based on two different tables
+  mutate(sector = case_when(between(TRL, 700,799) ~ "unemployed",
+    year(alta) > 2008 & between(CNAE_2009, 11, 99) ~ "agriculture",
+    year(alta) > 2008 & between(CNAE_2009, 501, 592) ~ "industrial",
+    year(alta) > 2008 & between(CNAE_2009, 101, 192) ~ "industrial",
+    year(alta) > 2008 & between(CNAE_2009, 301, 392) ~ "T&E",
+    year(alta) > 2008 & between(CNAE_2009, 401, 439) ~ "construction",
+    year(alta) > 2008 & between(CNAE_2009, 451, 479) ~ "services",
+    year(alta) > 2008 & between(CNAE_2009, 491, 495) ~ "T&E",
+    year(alta) > 2008 & between(CNAE_2009, 551, 639) ~ "services",
+    year(alta) > 2008 & between(CNAE_2009, 641, 683) ~ "finance",
+    year(alta) > 2008 & between(CNAE_2009, 691, 702) ~ "R&D",
+    year(alta) > 2008 & between(CNAE_2009, 711, 829) ~ "services",
+    year(alta) > 2008 & between(CNAE_2009, 841, 854) ~ "public",
+    year(alta) > 2008 & between(CNAE_2009, 861, 889) ~ "medical",
+    year(alta) > 2008 & between(CNAE_2009, 900, 960) ~ "services",
+    year(alta) > 2008 & between(CNAE_2009, 970, 982) ~ "domestic",
+    year(alta) > 2008 & CNAE_2009 == 990 ~ "public",
+    year(alta) <= 2008 & between(CNAE93, 10, 92) ~ "agriculture", 
+    year(alta) <= 2008 & between(CNAE93, 101, 343) ~ "industrial", 
+    year(alta) <= 2008 & between(CNAE93, 351, 455) ~ "construction", 
+    year(alta) <= 2008 & between(CNAE93, 501, 555) ~ "services", 
+    year(alta) <= 2008 & between(CNAE93, 601, 672) ~ "T&E", 
+    year(alta) <= 2008 & between(CNAE93, 701, 714) ~ "finance", 
+    year(alta) <= 2008 & between(CNAE93, 721, 726) ~ "services", 
+    year(alta) <= 2008 & between(CNAE93, 731, 732) ~ "R&D", 
+    year(alta) <= 2008 & between(CNAE93, 741, 752) ~ "services", 
+    year(alta) <= 2008 & between(CNAE93, 801, 804) ~ "public", 
+    year(alta) <= 2008 & between(CNAE93, 851, 853) ~ "medical", 
+    year(alta) <= 2008 & between(CNAE93, 900, 913) ~ "public", 
+    year(alta) <= 2008 & between(CNAE93, 921, 930) ~ "services", 
+    year(alta) <= 2008 & CNAE93 == 950 ~ "domestic", 
+    year(alta) <= 2008 & CNAE93 == 990  ~ "public", 
+    TRUE ~ "unknown")) %>% 
+  select(-TRL, -CNAE93, -CNAE_2009) %>% 
+  mutate(causa = if_else(is.na(baja), 98, causa),
+         age_at_start = time_length(interval(dob,alta),unit="years")) %>% 
+  arrange(IPF, alta)
+
+# here we eliminate instances of ere, and similar
+# as long as they are fully within an envelope of another spell
+# This is required as a pre-step. Best done w parallel processing
+# but not strictly necessary. This takes 6.5 minutes on ca 37000 trajectories (1m initial episodes)
+tic()
+cluster <- new_cluster(7)
+cluster_copy(cluster, "delete_contained_unemp")
+cluster_library(cluster,packages=c("tidyverse","lubridate"))
+LC2 <- 
+  LC1 %>% 
+  arrange(IPF,alta) %>% 
+  group_by(IPF) %>% 
+  partition(cluster) %>% 
+  do(delete_contained_unemp(X = .data)) %>% 
+  collect() %>% 
+  ungroup()
+toc()
+rm(cluster);gc()
+LC1 %>% nrow() / 
+LC2 %>% nrow()
+
+
+tic()
+cluster <- new_cluster(7)
+cluster_copy(cluster, "pad_inactivity")
+cluster_library(cluster,packages=c("tidyverse","lubridate"))
+LC_padded <- 
+  LC2 %>% 
+  arrange(IPF,alta) %>% 
+  group_by(IPF) %>% 
+  partition(cluster) %>% 
+  do(pad_inactivity(X = .data)) %>% 
+  collect() %>% 
+  ungroup()
+toc()
+
+rm(LC);gc()
+
+# which states follow unemp
+
+# if unemp is approx 1 yr and followed by temp %in% c("no consta", "labor gap") then merge to
+# form temp = "ltu".
+
+LC_padded %>% 
+  mutate(
+    baja = if_else(is.na(baja), dmy("31.12.2020"),baja),
+    duration =time_length(interval(alta,baja),unit="years")) %>% 
+  group_by(IPF) %>% 
+  mutate(keep_ = any(temp == "unemp" & duration > .97 & lead(occ) == "inactivity")) %>% 
+  ungroup() %>% 
+  dplyr::filter(keep_) %>% 
+  pull(IPF) %>% unique() %>% length()
+
+
+LC_padded$IPF %>% unique()
+tic()
+cluster <- new_cluster(7)
+cluster_copy(cluster, "discretize_trajectory")
+cluster_library(cluster,"tidyverse")
+random_draws <- sample(LC_padded$IPF %>% unique(), size = 1000, replace = FALSE)
+LC_discrete <- 
+  LC_padded %>% 
+  # dplyr::filter(IPF %in% random_draws) %>% 
+  mutate(alta = decimal_date(alta),
+         baja = decimal_date(baja),
+         baja = ifelse(is.na(baja),2021, baja)) %>% 
+  dplyr::filter(baja > alta) %>% 
+  arrange(IPF,alta) %>% 
+  group_by(IPF) %>% 
+  partition(cluster) %>% 
+  do(discretize_trajectory(X = .data, step_size = .5)) %>% 
+  collect() %>% 
+  ungroup()
+toc()
+rm(cluster);gc()
+# 12.5 minutes for 1000 on 7 cores.
+# 7.367425 hours 37147 that were matched to 
+# the first 1 million cotizaciones
+readr::write_csv(LC_discrete, file = "Data/LC_discrete.csv")
+LC_discrete$IPF %>% unique() %>% length()
+LC_discrete <- read_csv("Data/LC_discrete.csv")
+# using quarters only implies 15% growth in rows vis a vis
+# the original date (exact) trajectories. An acceptable penalty
+nrow(LC_discrete) / nrow(LC_padded)
+
+remotes::install_github("timriffe/Spells/R/Spells", force = TRUE)
+# test applying alignments
+library(Spells)
+LC_aligned <- 
+  LC_discrete %>% 
+  group_by(IPF) %>% 
+  mutate(next_temp = lead(temp),
+         keep_ = any(start > 1997 & next_temp == "perm" & temp != "perm")) %>% 
+  ungroup() %>% 
+  dplyr::filter(keep_) %>% 
+  group_by(IPF) %>% 
+  mutate(first_perm = Spells::align(temp, state = "perm", type = "left", spell = "first", step_size = .25),
+         longest_perm = Spells::align(temp,  state = "perm", type = "left", spell = "longest", step_size = .25))
+
+
+LC_aligned %>% 
+  mutate(coh10 = year(dob) - year(dob) %% 10) %>% 
+  dplyr::filter(between(coh10, 1940, 1980)) %>% 
+  group_by(IPF) %>% 
+  mutate(period = start[first_perm == 0] > 2008) %>% 
+  group_by(period, sex,first_perm) %>% 
+  summarize(n = n(),
+            labor_gap = sum(temp == "labor gap") / n(),
+            temp_job = sum(temp == "temp") / n(),
+            care_gap = sum(temp == "care gap") / n(),
+            unemp = sum(temp == "unemp") / n(),
+            retired = sum(temp == "retirement") / n(),
+            perm = sum(temp == "perm") / n(),
+            .groups = "drop") %>% 
+  pivot_longer(labor_gap:perm, names_to = "temp", values_to = "prev") %>% 
+  # filter(first_perm >= 0) %>% 
+  ggplot(aes(x = first_perm, y = prev, color = temp, group = temp)) +
+  geom_line() +
+  facet_grid(vars(period), vars(sex))
+  # filter(decade == 2020, coh10 == 1980) %>% View()
+
+random_draws_m <- LC_padded %>% filter(sex == 1) %>% pull(IPF) %>% sample(100, replace=FALSE)
+pm <- 
+LC_discrete %>% 
+  dplyr::filter(IPF %in% random_draws_m) %>% 
+  plot_traj_2() +
+  labs(title = "100 randomly selected men")+
+  theme_minimal()
+
+random_draws_f <- LC_padded %>% filter(sex == 2) %>% pull(IPF) %>% sample(100, replace=FALSE)
+pf <-
+LC_discrete %>% 
+  dplyr::filter(IPF %in% random_draws_f) %>% 
+  plot_traj_2()  +
+  labs(title = "100 randomly selected women") +
+  theme_minimal()
+plot_grid(pm,pf)
+  
+LC_padded %>% 
+  dplyr::filter(IPF %in% random_draws) %>% 
+  plot_traj_1()
+# note example case 31012
+LC_discrete %>% 
+  group_by(IPF) %>% 
+  mutate(has_dead = any(temp =="dead")) %>% 
+  dplyr::filter(has_dead) %>% 
+  plot_traj_2() 
+
+
+
+LC %>% 
+  group_by(IPF) %>% 
+  mutate(keep_ = any(temp == "perm"),
+         age = decimal_date(alta) - decimal_date(dob),
+         age5 = age - age %% 5,
+         coh5 = year(dob) - year(dob) %% 5) %>% 
+  ungroup() %>% 
+  dplyr::filter(keep_) %>% 
+  group_by(IPF) %>% 
+  summarize(ttp = (alta[temp == "perm"] %>% min() %>% decimal_date()) -
+            (alta %>% min() %>% decimal_date()), 
+            age5 = age5[temp == "perm"][which.min(decimal_date(alta)[temp == "perm"])],
+            sex =sex[1],
+            coh5 = coh5[1],
+            ttpmax = 2021 - (alta %>% min() %>% decimal_date()),
+            .groups = "drop") %>% 
+  dplyr::filter(coh5 < 1990) %>% 
+  mutate(ttp = floor(ttp)) %>% 
+  ggplot(aes(x = ttp, color = coh5, fill = coh5)) +
+  geom_bar(position = "stack")
+
+
+
+
+
+LC %>% 
+  ggplot(aes(x = alta, y = dob)) +
+  geom_density2d_filled() +
+  facet_wrap(~temp)
+
+LC %>% 
+  filter(IPF == 32) %>% 
+  arrange(alta) %>% 
+  View()
+
+
+  filter(temp == "perm") %>% 
+  ggplot(aes(x = duration)) +
+  geom_density() +
+  facet_wrap(~age5) +
+  xlim(0,12)
+
+LC %>% 
+  group_by(IPF) %>% 
+  summarize(turb = n() / length(unique(CCC)),
+            n = decimal_date(max(baja)) - decimal_date(min(alta)), .groups = "drop") %>% 
+  ggplot(aes(x=n)) +
+  geom_density() +
+  scale_x_log10()
+X <- LC %>% 
+  filter(IPF == 32)
+
+random_draws <- 
+LC %>% 
+  pull(IPF) %>% unique() %>% sample(size = 1, replace = FALSE)
+
+# X <-
+# LC %>% 
+#   filter(IPF %in% random_draws) 
+  
+
+pad_inactivity(X) %>% 
+  plot_traj()
+
+
+
+# discretize trajectory
+X <- 
+  LC_padded %>% 
+  filter(IPF %in% random_draws) %>% 
+  mutate(alta = decimal_date(alta),
+         baja = decimal_date(baja))
+
+step_size <- 1/4
+
+orig <- 
+  X %>% 
+  plot_traj_1() +
+  theme_void() +
+  xlim(1967,2013)
+
+d.1 <-
+  discretize_trajectory(X, .1) %>% 
+  plot_traj_2() +
+  guides(fill = "none") +
+  theme_void()+
+  xlim(1967,2013)
+
+d.25 <- 
+  discretize_trajectory(X, .25) %>% 
+  plot_traj_2() +
+  guides(fill = "none") +
+  theme_void() +
+  xlim(1967,2013)
+d.5 <- 
+discretize_trajectory(X, .5) %>% 
+  plot_traj_2() +
+  guides(fill = "none") +
+  theme_void() +
+  xlim(1967,2013)
+d1 <-
+discretize_trajectory(X, 1) %>% 
+  plot_traj_2()+
+  guides(fill = "none") +
+  theme_void()+
+  xlim(1967,2013)
+pcol <- plot_grid(
+  orig + guides(fill = "none"),
+  d.1, 
+  d.25, 
+  d.5, 
+  d1,
+  ncol = 1
+)
+
+legend_b <- get_legend(
+  orig + 
+    guides(color = guide_legend(nrow = 1)) +
+    theme(legend.position = "bottom")
+)
+pcol <-
+  pcol +
+  draw_plot_label(label = c("exact","0.1","0.25","0.5","1"),
+                  x = rep(0.05,5),
+                  y = (rep(.9,1) + 4:0)/ 5,
+                  hjust = 0,
+                  vjust = .5)
+library(cowplot)
+plot_grid(pcol, legend_b, ncol = 1, rel_heights = c(1, .15))
---
+++ functions.R
@@ -0,0 +1,276 @@
+
+# pacman will help us get everything installed
+if (!"pacman" %in% rownames(installed.packages())){
+  install.packages("pacman")
+}
+library(pacman)
+CRAN_packages <- c("haven","tidyverse","lubridate","tictoc","multidplyr","readr")
+
+# Install required CRAN packages if not available yet
+if(!sum(!p_isinstalled(CRAN_packages))==0) {
+  p_install(
+    package = CRAN_packages[!p_isinstalled(CRAN_packages)], 
+    character.only = TRUE
+  )
+}
+
+# install from github 
+if (!p_isinstalled("Spells")) {
+  remotes::install_github("timriffe/Spells/R/Spells", build = FALSE)
+}
+
+# Load the required CRAN/github packages
+p_load(CRAN_packages, character.only = TRUE)
+library(Spells)
+
+# TODO create new category called "long term unemployment",
+# 1) spells of unempl > 1 year
+# 2) spells of unempl followed by inactivity > 1 year total
+# (same thing in 2 steps: first merge then check)
+# unemp causa: 
+# 54 benefit ran up; 
+# 51 voluntary stop; 
+# 65 temporary sick/ disabled
+
+# we should delete unemployment that is within a work spell.
+delete_contained_unemp <- function(X){
+  # cases of this are prevalent in 2021 and 2020,
+  # interesting in general, but not for this study.
+  X %>% 
+    # sort episodes
+    arrange(alta) %>% 
+    mutate(
+      baja = if_else(causa == 98, dmy("31.12.2020"), baja),
+      last_baja1 = lag(baja),
+      last_baja2 = lag(baja,2),
+      last_baja3 = lag(baja,3),
+      last_baja4 = lag(baja,4),
+      last_baja5 = lag(baja,5),
+      last_baja6 = lag(baja,6),
+      last_baja7 = lag(baja,7),
+      last_baja8 = lag(baja,8),
+      max_earlier_baja = pmax(last_baja1,last_baja2,last_baja3,last_baja4,last_baja5,last_baja6,last_baja7,last_baja8,na.rm=TRUE)) %>% 
+    # we know last_alta < alta, but if 
+    dplyr::filter(temp == "unemp",
+                  baja > max_earlier_baja) %>% 
+    select(any_of(colnames(X))) %>% 
+    bind_rows(dplyr::filter(X, temp != "unemp")) %>% 
+    arrange(alta)
+}
+
+
+pad_inactivity <- function(X){
+  INAC <- 
+    X %>% 
+    # sort episodes
+    arrange(alta) %>% 
+    # detect gaps between episodes based on lag between 
+    # current baja and next alta. Positives are gaps.
+    # negatives are overlapping episodes which we don't handle here
+    mutate(# gap = lead(alta) - baja,
+      # test
+           gap = time_length(interval(baja,lead(alta)),unit="years"),
+           # special case for death = 1 day event
+           gap = ifelse(causa == 56, 1, gap),
+           next_alta = baja + gap,
+           next_CCC = lead(CCC)) %>% 
+    # only keep breaks with positive durations
+    dplyr::filter(gap > 0) 
+  if(nrow(INAC) > 0){
+    INAC <- 
+      INAC %>% 
+      mutate(alta = baja,
+             baja = next_alta,
+             # if it's a within-company break, then keep company id
+             CCC = ifelse(CCC == next_CCC, CCC, "999"),
+             part = "full",
+             sector = "inactivity",
+             occ = "inactivity",
+             # assign inactivity state based on "cause" of termination 
+             # of last episode.
+             temp = case_when(
+               causa == 58 ~ "retirement",
+               causa %in% c(68,73) ~ "care gap",
+               causa == 56 ~ "dead",
+               causa %in% c(51,52,54,55,60,65,69,74,77,91,92,93,94) ~ "labor gap",
+               causa == 99 ~ "other"
+             ),
+             duration = gap,
+             age_at_start = decimal_date(alta) - decimal_date(dob))
+  }
+  INAC %>% 
+    select(any_of(colnames(X))) %>% 
+    # append to incoming data
+    bind_rows(X) %>% 
+    arrange(alta)
+}
+
+
+# This is the function that determines the state for each uniform discrete time step
+# it needs a new states called "precarious" and "instability" or similar, defined on the basis
+# instability: nr transitions
+# precarious: short employment, under full time, unskilled, low pay
+# long term unemployment
+discretize_trajectory <- function(X, step_size = 1/4){
+  
+  end   <- X %>% 
+    pull(baja) %>% 
+    max() %>% 
+    '*'(1/step_size) %>% 
+    ceiling() %>% 
+    '*'(step_size)
+  
+  start <- X %>% 
+    pull(alta) %>% 
+    min() %>% 
+    '*'(1/step_size) %>% 
+    floor() %>% 
+    '*'(step_size)
+  
+  starts <- seq(start, end-step_size, by = step_size)
+  
+  Y <-
+    tibble(IPF = X$IPF[1],
+           dob = X$dob[1],
+           sex = X$sex[1],
+           dod = X$dod[1],
+           start = starts,
+           occ = NA_character_,
+           temp = NA_character_,
+           part = NA_character_,
+           sector = NA_character_) %>% 
+    mutate(end = start + step_size, .after = start)
+  
+  for (i in 1:nrow(Y)){
+    XX <- X %>% 
+      mutate(left_overlap = between(alta, Y$start[i], Y$end[i]),
+             right_overlap = between(baja, Y$start[i], Y$end[i]),
+             bin_within_episode = alta < Y$start[i] & baja > Y$end[i],
+             episode_within_bin = Y$start[i] < alta & Y$end[i] > baja) %>% 
+      dplyr::filter(left_overlap |
+                      right_overlap |
+                      bin_within_episode |
+                      episode_within_bin) %>% 
+      mutate(alta = ifelse(alta < Y$start[i], Y$start[i], alta),
+             baja = ifelse(baja > Y$end[i], Y$end[i], baja),
+             duration = baja - alta)
+    
+    if (any(XX$temp == "dead")){
+      Y$occ[i]    <- "dead"
+      Y$temp[i]   <- "dead"
+      next
+    }
+    # case 1, if there is one and only one row with temp == perm,
+    # we keep its information and toss the rest
+    if (any(XX$temp == "perm")){
+      XX <- XX %>% dplyr::filter(temp == "perm")
+      
+      # if there's more than 1 permanent job in the interval (really?), 
+      # then keep the one that was in the interval for the longest
+      if (nrow(XX) > 1){
+        XX <-
+          XX %>% 
+          dplyr::filter(duration == max(duration))
+      }
+    }
+    
+    # case 2 if some episode takes up > .5 bin width, then we take its characteristics
+    if (any(XX$duration > (step_size / 2))){
+      XX <-
+        XX %>% 
+        dplyr::filter(duration == max(duration))
+    }
+    
+    # case 3, if there is only one intersecting episode,
+    # we carry over its info.
+    if (nrow(XX) == 1){
+      Y$occ[i]    <- XX$occ
+      Y$temp[i]   <- XX$temp
+      Y$part[i]   <- XX$part
+      Y$sector[i] <- XX$sector
+      next
+    }
+    
+    # case 4 
+    Y$occ[i] <- XX %>% 
+      group_by(occ) %>% 
+      summarize(duration = sum(duration)) %>% 
+      dplyr::filter(duration == max(duration)) %>% 
+      dplyr::pull(occ) %>% 
+      first()
+    Y$temp[i] <- XX %>% 
+      group_by(temp) %>% 
+      summarize(duration = sum(duration)) %>% 
+      dplyr::filter(duration == max(duration)) %>% 
+      dplyr::pull(temp) %>% 
+      first()
+    Y$part[i] <- XX %>% 
+      group_by(part) %>% 
+      summarize(duration = sum(duration)) %>% 
+      dplyr::filter(duration == max(duration)) %>% 
+      dplyr::pull(part) %>% 
+      first()
+    Y$sector[i] <- XX %>% 
+      group_by(sector) %>% 
+      summarize(duration = sum(duration)) %>% 
+      dplyr::filter(duration == max(duration)) %>% 
+      dplyr::pull(sector) %>% 
+      first()
+    next
+  }
+  Y
+}
+
+
+# helper for conceptualizing
+plot_traj_1 <- function(X){
+  X %>% 
+    group_by(IPF) %>% 
+    mutate(first = min(alta)) %>% 
+    ungroup() %>% 
+    arrange(first, alta) %>% 
+    group_by(first, IPF) %>% 
+    mutate(ymin = cur_group_id() - 1,
+           ymax = ymin + 1) %>%  
+    ggplot(aes(xmin = alta, 
+               xmax = baja, 
+               ymin = ymin, 
+               ymax = ymax, 
+               fill = temp))  +
+    geom_rect() +
+    scale_fill_manual(values =c("no consta" = gray(.8), 
+                                "labor gap" = "#ebc567", 
+                                "unemp" = "#cf8908", 
+                                "perm" = "#a368a8",
+                                "temp" = "#ed6639",
+                                "other" = "#b9c97f",
+                                "retirement" = "#9ed9d4",
+                                "care gap" = "#1952a8",
+                                "dead" = "red"))
+}
+
+plot_traj_2 <- function(X){
+  X %>% 
+    group_by(IPF) %>% 
+    mutate(first = min(start)) %>% 
+    ungroup() %>% 
+    arrange(first, start) %>% 
+    group_by(first, IPF) %>% 
+    mutate(ymin = cur_group_id() - 1,
+           ymax = ymin + 1) %>%  
+    ggplot(aes(xmin = start, 
+               xmax = end, 
+               ymin = ymin, 
+               ymax = ymax, 
+               fill = temp))  +
+    geom_rect() +
+    scale_fill_manual(values =c("no consta" = gray(.8), 
+                        "labor gap" = "#ebc567", 
+                        "unemp" = "#cf8908", 
+                        "perm" = "#a368a8",
+                        "temp" = "#ed6639",
+                        "other" = "#b9c97f",
+                        "retirement" = "#9ed9d4",
+                        "care gap" = "#1952a8",
+                        "dead" = "red"))
+}
